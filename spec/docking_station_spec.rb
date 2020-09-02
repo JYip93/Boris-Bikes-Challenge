b2d21dd477c5f0b5033d38568f1b558ce8f1ae56
Veronica Lee <veronicavlee@icloud.com>
2020-09-02 16:11:12 +0100

    Add release_bike method and failing test for DockingStation responding to it

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,5 +1,7 @@
 require 'DockingStation_class'
 
   describe DockingStation do
-
+    describe "release bike from docking station" do
+      it { is_expected.to respond_to(release_bike) }
+    end
   end
