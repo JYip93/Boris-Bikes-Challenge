c3ca00b6a28993d25a497271f509de120e34592a
Johnny Yip <jyippy2012@gmail.com>
2020-09-03 16:42:32 +0100

    Raising errors

--- lib/DockingStation_class.rb
+++ lib/DockingStation_class.rb
@@ -1,17 +1,29 @@
 class DockingStation
 
   attr_reader :docked_bike
+ 
+  def initialize
+@capacity = 1
+  end
+
 
   def release_bike
-    if @docked_bike = bike
-      nil
-    else
+    if @capacity == 1
     @bike = Bike.new
+    @capacity = 0
+    @bike
+    else
+      raise "no more space"
     end
   end
 
 def dock(bike)
+  if @capacity == 0
     @docked_bike = bike
+    @capacity = 1
+  else
+    raise "It's full"
+  end
   end
 
 
